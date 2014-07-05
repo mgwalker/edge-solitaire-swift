e5e1aee8441cc18fc33f9ba332b1fe0acf3e151e
darkcooger <cooger@darkcooger.net>
2014-07-05 00:09:45 -0500

    Figure out cell sizes, spacing, etc., based on the constrained size of the collection view

--- Edge Solitaire/GameViewController.swift
+++ Edge Solitaire/GameViewController.swift
@@ -36,6 +36,43 @@ class GameViewController:UIViewController,UICollectionViewDataSource,UICollectio
 		self.nextCard.image = CardHelper.imageForCard(Card(suit: Card.Suit.Club, rank: Card.Rank.Eight));
 	}
 	
+	override func viewDidLayoutSubviews()
+	{
+		super.viewDidLayoutSubviews();
+
+		let layout = self.cardCollection.collectionViewLayout as UICollectionViewFlowLayout;
+		
+		var containerSize = self.cardCollection.bounds.size;
+		var itemSize = CGSize(width: 0, height: 0);
+		var itemSpacing:CGFloat = 0;
+		var lineSpacing:CGFloat = 0;
+
+		if containerSize.width * 1.4 < containerSize.height
+		{
+			itemSpacing = 10;
+			itemSize.width = (containerSize.width - (itemSpacing * 5)) / 4;	// left/right insets, plus three spacers
+			itemSize.height = itemSize.width * 1.4;
+			
+			lineSpacing = (containerSize.height - (itemSize.height * 4)) / 5;
+		}
+		else
+		{
+			lineSpacing = 10;
+			itemSize.height = (containerSize.height - (lineSpacing * 5)) / 4; // top/right insets, plus three spacers
+			itemSize.width = itemSize.height / 1.4;
+			
+			itemSpacing = (containerSize.width - (itemSize.width * 4)) / 5;
+		}
+		
+		let hInset = (containerSize.width - ((itemSize.width * 4) + (itemSpacing * 3))) / 2.0;
+		let vInset = (containerSize.height - ((itemSize.height * 4) + (lineSpacing * 3))) / 2.0;
+		
+		layout.itemSize = itemSize;
+		layout.minimumInteritemSpacing = itemSpacing;
+		layout.minimumLineSpacing = lineSpacing;
+		layout.sectionInset = UIEdgeInsets(top: vInset, left: hInset, bottom: vInset, right: hInset);
+	}
+	
 	func numberOfSectionsInCollectionView(collectionView: UICollectionView!) -> Int
 	{
 		return 1;
