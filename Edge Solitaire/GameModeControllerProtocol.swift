40434859eb4c63b96ec0972a5cb47e9e05b4807c
darkcooger <cooger@darkcooger.net>
2014-07-05 12:35:41 -0500

    More card drawing!

--- Edge Solitaire/GameModeControllerProtocol.swift
+++ Edge Solitaire/GameModeControllerProtocol.swift
@@ -74,16 +74,19 @@ class CardHelper
 {
 	class func imageForCard(card:Card) -> UIImage
 	{
-		let suitColor = (card.suit == Card.Suit.Diamond || card.suit == Card.Suit.Heart ? UIColor.redColor() : UIColor.blackColor());
+		let suitColor = (card.suit == Card.Suit.Diamond || card.suit == Card.Suit.Heart ?
+			UIColor(red: 0.75, green: 0, blue: 0, alpha: 1.0) : UIColor.blackColor());
 		
-		//let template = UIImage.tintedImage(named: "Card Template", tint:suitColor);
-		let template = UIImage(named: "Card Template");
+		let blank = UIImage.tintedImage(named: "Card Blank", tint: UIColor.whiteColor());
+		let template = UIImage.tintedImage(named: "Card Template", tint:suitColor);
+		//let template = UIImage(named: "Card Template");
 		let suit = UIImage.tintedImage(named: "Suit - \(card.suit.toRaw())", tint:suitColor);
 		let rank = UIImage.tintedImage(named: "Rank - \(card.rank.toRaw())", tint:UIColor.whiteColor());
 		
 		UIGraphicsBeginImageContext(template.size);
 		let context = UIGraphicsGetCurrentContext();
 		
+		blank.drawAtPoint(CGPoint(x: 0, y: 0));
 		template.drawAtPoint(CGPoint(x: 0, y: 0));
 		
 		var rankX = 75 - (rank.size.width / 2.0);
@@ -94,9 +97,75 @@ class CardHelper
 		rankY = template.size.height - rankY - rank.size.height;
 		rank.drawAtPoint(CGPoint(x: rankX, y: rankY));
 		
-		var suitX = (template.size.width - suit.size.width) / 2.0;
-		var suitY = (template.size.height - suit.size.height) / 2.0;
-		suit.drawAtPoint(CGPoint(x: suitX, y: suitY));
+		let rankValue = card.rank.toRaw();
+		
+		if rankValue > 1 && rankValue < 11
+		{
+			let suitWidth = suit.size.width;
+			let suitHalfWidth = suitWidth / 2.0;
+			let suitHeight = suit.size.height;
+			let suitHalfHeight = suitHeight / 2.0;
+			let centerX = (template.size.width - suitWidth) / 2.0;
+			let centerY = (template.size.height - suitHeight) / 2.0;
+			
+			if rankValue == 3 || rankValue == 5 || rankValue == 9
+			{
+				// center, center
+				suit.drawAtPoint(CGPoint(x: centerX, y: centerY));
+			}
+			if rankValue == 2 || rankValue == 3
+			{
+				// center, top and bottom
+				suit.drawAtPoint(CGPoint(x: centerX, y: centerY - suitHeight - suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX, y: centerY + suitHeight + suitHalfHeight));
+			}
+			if rankValue == 5 || rankValue == 4
+			{
+				// corners
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY - suitHeight - suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY - suitHeight - suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY + suitHeight + suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY + suitHeight + suitHalfHeight));
+			}
+			if rankValue == 6 || rankValue == 7
+			{
+				// columns of three
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY - suitHeight - suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY - suitHeight - suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY));
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY + suitHeight + suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY + suitHeight + suitHalfHeight));
+			}
+			if rankValue == 7
+			{
+				suit.drawAtPoint(CGPoint(x: centerX, y: centerY - suitHalfHeight));
+			}
+			if rankValue == 8 || rankValue == 9 || rankValue == 10
+			{
+				// columns of four
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY - suitHalfHeight - suitHeight));
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY - suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY + suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX - suitWidth, y: centerY + suitHalfHeight + suitHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY - suitHalfHeight - suitHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY - suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY + suitHalfHeight));
+				suit.drawAtPoint(CGPoint(x: centerX + suitWidth, y: centerY + suitHalfHeight + suitHeight));
+			}
+			if rankValue == 10
+			{
+				suit.drawAtPoint(CGPoint(x: centerX, y: centerY - suitHeight));
+				suit.drawAtPoint(CGPoint(x: centerX, y: centerY + suitHeight));
+			}
+		}
+		else if card.rank == Card.Rank.Ace
+		{
+			let ace = UIImage.tintedImage(named: "Face - 1\(card.suit.toRaw())", tint: suitColor);
+			ace.drawAtPoint(CGPoint(
+				x: (template.size.width - ace.size.width) / 2.0,
+				y: (template.size.height - ace.size.height) / 2.0));
+		}
 		
 		let final = UIGraphicsGetImageFromCurrentImageContext();
 		UIGraphicsEndImageContext();
